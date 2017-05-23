4079a24a6ec5044970849e63d5afff09cfde973f
manish.khanna <manish.khanna@in.ibm.com>
2017-05-23 23:33:00 +0530

    restucture

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
 const yargs = require('yargs');
 
-const geocodeLib = require('./geocode/geocode');
+const geocodeLib = require('./lib/geocode');
+const weatherLib = require('./lib/weather');
+
 
 // Get location address 
 const argv = yargs
@@ -17,20 +19,22 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-geocodeLib.geocodeAddress(argv.address, (geocodeErrorMessage, results) => {
+geocodeLib.getGeocode(argv.address, (geocodeErrorMessage, results) => {
 // first get lat,long for given address
 // prepare the URL and pass to forecast.io
     if (geocodeErrorMessage) {
         console.log(geocodeErrorMessage);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        // console.log(JSON.stringify(results, undefined, 2));
         
 // now that we got geocode, get the weather
-        geocodeLib.forecastAddress(results.forecastURL, (forecastErrorMessage, forecastResults) => {
+        weatherLib.getWeather(results.longitude, results.latitude, (forecastErrorMessage, weatherResults) => {
             if (forecastErrorMessage) {
                 console.log(forecastErrorMessage);
             } else {
-                console.log(JSON.stringify(forecastResults, undefined, 2));
+                // console.log(JSON.stringify(forecastResults, undefined, 2));
+                console.log(`Today's temperature is ${weatherResults.temperature}. in ${results.formatted_address}`);
+                console.log(`Feels like ${weatherResults.apparentTemperature}`);
             }
         });
     }
